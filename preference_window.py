ea8630a3c7a40ec53df54991347bb47a4c23c992
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2020-11-17 01:43:44 +0900

    Update: create_*_progresses

--- preference_window.py
+++ preference_window.py
@@ -22,43 +22,43 @@ class Window(ttk.Frame):
     def create_variables(self):
         pass
 
-    def create_frame_basics(self):
+    def create_frame_basics(self) -> None:
         self.frame_basics = ttk.Frame()
         self.create_widgets_basics()
         self.create_layout_basic()
         self.frame_basics.pack()
 
-    def create_frame_energies(self):
+    def create_frame_energies(self) -> None:
         self.frame_energies = ttk.Frame()
         self.create_widgets_energies()
         self.create_layout_energies()
         self.frame_energies.pack()
 
-    def create_frame_checks(self):
+    def create_frame_checks(self) -> None:
         self.frame_checks = ttk.Frame()
         self.create_widgets_checks()
         self.create_layout_checks()
         self.frame_checks.pack()
 
-    def create_frame_memos(self):
+    def create_frame_memos(self) -> None:
         self.frame_records = ttk.Frame()
         self.create_widgets_records()
         self.create_layout_records()
         self.frame_records.pack()
 
-    def create_frame_buttons(self):
+    def create_frame_buttons(self) -> None:
         self.frame_buttons = ttk.Frame()
         self.create_widgets_buttons()
         self.create_layout_buttons()
         self.frame_buttons.pack()
 
-    def create_frame_bar(self):
+    def create_frame_bar(self) -> None:
         self.frame_bars = ttk.Frame()
         self.create_widgets_progresses()
         self.create_layout_progresses()
         self.frame_bars.pack()
 
-    def create_widgets_basics(self):
+    def create_widgets_basics(self) -> None:
         # The first row
         self.n_cell_label = ttk.Label(self.frame_basics, text="Number of cell")
         self.n_cell = ttk.Entry(self.frame_basics, width=7)
@@ -92,7 +92,7 @@ class Window(ttk.Frame):
         self.prefactor = ttk.Entry(self.frame_basics, width=7)
         self.prefactor.insert(tk.END, "1e13")
 
-    def create_layout_basic(self):
+    def create_layout_basic(self) -> None:
         """Frame for number of cell, Z unit, ..."""
         # the first row
         self.n_cell_label.grid(row=0, column=0, **self.padWE)
@@ -113,7 +113,7 @@ class Window(ttk.Frame):
         self.prefactor_label.grid(row=1, column=6, **self.padWE)
         self.prefactor.grid(row=1, column=7, **self.padWE)
 
-    def create_widgets_energies(self):
+    def create_widgets_energies(self) -> None:
         labels: List[str] = [
             "  ",
             "Ag-Si",
@@ -136,7 +136,7 @@ class Window(ttk.Frame):
         for label in labels:
             self.energylabels.append(ttk.Label(self.frame_energies, text=label))
 
-    def create_layout_energies(self):
+    def create_layout_energies(self) -> None:
         for i, energylabel in enumerate(self.energylabels):
             energylabel.grid(row=0, column=i, **self.padWE)
         self.energy_label0 = ttk.Label(self.frame_energies, text="Energy  (eV)")
@@ -146,7 +146,7 @@ class Window(ttk.Frame):
         self.rate_bond_label = ttk.Label(self.frame_energies, text="Rates/bond")
         self.rate_bond_label.grid(row=2, **self.padWE)
 
-    def create_widgets_checks(self):
+    def create_widgets_checks(self) -> None:
         self.bln_tr = tk.BooleanVar()
         self.bln_tr.set(True)
         self.chk_transformation_label = ttk.Label(
@@ -164,14 +164,14 @@ class Window(ttk.Frame):
         )
         self.chk_defect = ttk.Checkbutton(self.frame_checks, variable=self.bln_defect)
 
-    def create_layout_checks(self):
+    def create_layout_checks(self) -> None:
         self.chk_transformation_label.grid(row=0, column=0, **self.padWE)
         self.chk_transformation.grid(row=0, column=1, **self.padWE)
         self.transformation.grid(row=0, column=2, **self.padWE)
         self.chk_defect_label.grid(row=1, column=0, **self.padWE)
         self.chk_defect.grid(row=1, column=1, **self.padWE)
 
-    def create_widgets_records(self):
+    def create_widgets_records(self) -> None:
         self.record_label = tk.Label(self.frame_records, text="Record")
         self.record = tk.Entry(self.frame_records, width=50)
         self.record.insert(tk.END, "kMC_rec")
@@ -182,7 +182,7 @@ class Window(ttk.Frame):
         self.comments = tk.Entry(self.frame_records, width=80)
         self.comments.insert(tk.END, "No comments")
 
-    def create_layout_records(self):
+    def create_layout_records(self) -> None:
         self.record_label.grid(row=0, column=0, **self.padWE)
         self.record.grid(row=0, column=1, **self.padWE)
         self.image_rec_label.grid(row=0, column=2, **self.padWE)
@@ -190,32 +190,48 @@ class Window(ttk.Frame):
         self.comments_label.grid(row=1, column=0, **self.padWE)
         self.comments.grid(row=1, column=1, columnspan=3, **self.padWE)
 
-    def create_widgets_buttons(self):
+    def create_widgets_buttons(self) -> None:
         self.start = tk.Button(self.frame_buttons, text="Start", command=None, width=20)
         self.close = tk.Button(
             self.frame_buttons, text="close", command=self.close_function, width=20
         )
 
-    def create_layout_buttons(self):
+    def create_layout_buttons(self) -> None:
         self.start.grid(row=0, column=0)
         self.close.grid(row=0, column=1)
 
-    def create_widgets_progresses(self):
+    def create_widgets_progresses(self) -> None:
         self.pbval = 0
         self.progress_label = ttk.Label(self.frame_bars, text="Progress...")
         self.progress_bar = ttk.Progressbar(
             self.frame_bars,
             orient=tk.HORIZONTAL,
-            length=350,
+            length=650,
             mode="determinate",
         )
         self.progress_bar.configure(maximum=100, value=self.pbval)
+        self.time_label = tk.Label(self.frame_bars, text="time (sec)")
+        self.time = tk.Label(self.frame_bars, text="tobe..")
+        self.event_i_label = tk.Label(self.frame_bars, text="N of events")
+        self.event_i = tk.Label(self.frame_bars, text="imple")
+        self.n_atoms_label = tk.Label(self.frame_bars, text="N of atoms")
+        self.n_atoms = tk.Label(self.frame_bars, text="ment")
+        self.coverage_label = tk.Label(self.frame_bars, text="Coverage (ML)")
+        self.coverage = tk.Label(self.frame_bars, text="...")
 
-    def create_layout_progresses(self):
-        self.progress_label.grid(row=0, pady=15)
-        self.progress_bar.grid(pady=20)
+    def create_layout_progresses(self) -> None:
+        self.progress_label.grid(row=0, columnspan=5, pady=15)
+        self.progress_bar.grid(row=1, columnspan=5, pady=10)
+        self.time_label.grid(row=2, column=0, **self.padWE)
+        self.time.grid(row=3, column=0, **self.padWE)
+        self.event_i_label.grid(row=2, column=1, **self.padWE)
+        self.event_i.grid(row=3, column=1, **self.padWE)
+        self.n_atoms_label.grid(row=2, column=2, **self.padWE)
+        self.n_atoms.grid(row=3, column=2, **self.padWE)
+        self.coverage_label.grid(row=2, column=3, **self.padWE)
+        self.coverage.grid(row=3, column=3, **self.padWE)
 
-    def close_function(self):
+    def close_function(self) -> None:
         # plt.close("all")  <<< FIXME
         self.quit()
 
