1c15b03464076ffc3868bd62132a462c63f1cee2
k708 <k708abc@yahoo.co.jp>
2020-11-18 23:18:22 +0900

    "InputParameter" is  merged

--- preference_window.py
+++ preference_window.py
@@ -8,38 +8,14 @@ from cal_rates import rate
 from calculation import cal_start
 import time
 from lattice_form import lattice_form, lattice_visual
-
-init_values: Dict = dict(
-    n_cell_init = 5, 
-    z_unit_init = 5,
-    temperature = 550,
-    dep_rate = 0.4,
-    dep_time = 5,
-    post_anneal = 0,
-    prefactor = "1E+13",
-    AgSi = -1.4,
-    Si12 = -1.3,
-    Si23 = -1.4,
-    Si34 = -1.4,
-    Si45 = -1.2,
-    Si56 = -1.4,
-    Si_intra = -1.4,
-    Si_inter = -1.4,
-    Ag_top = -1.4,
-    transformation = -0.3,
-    record_name = "KMC_Si_rec",
-    img_per = 10,
-    comments = "No comments",
-    intra_distance = 0.25,
-    inter_distance = 0.22
-)
-
+from InputParameter import Params
 
 class Window(ttk.Frame):
     kb_eV = 8.617e-5
     padWE: Dict = dict(sticky=(tk.W, tk.E), padx=15, pady=2)
     def __init__(self, master):
         super().__init__(master, padding=2)
+        self.init_value = Params()
         self.create_variables()
         self.create_frame_basics()
         self.create_frame_energies()
@@ -107,41 +83,40 @@ class Window(ttk.Frame):
         # The first row
         self.n_cell_label = ttk.Label(self.frame_basics, text="Number of cell")
         self.n_cell = ttk.Entry(self.frame_basics, width=7)
-        self.n_cell.insert(tk.END, init_values["n_cell_init"])
+        self.n_cell.insert(tk.END, self.init_value.n_cell_init)
         self.n_cell.bind("<Return>", self.update_click)
         self.z_unit_label = ttk.Label(self.frame_basics, text="Z unit")
         self.z_unit = ttk.Entry(self.frame_basics, width=7)
-        self.z_unit.insert(tk.END, init_values["z_unit_init"])
+        self.z_unit.insert(tk.END, self.init_value.z_unit_init)
         self.z_unit.bind("<Return>", self.update_click)
         self.temperature_label = ttk.Label(self.frame_basics, text="T (K)")
         self.temperature = ttk.Entry(self.frame_basics, width=7)
-        self.temperature.insert(tk.END, init_values["temperature"])
+        self.temperature.insert(tk.END, self.init_value.temperature)
         self.temperature.bind("<Return>", self.update_click)
         self.temperautre_energy_label = ttk.Label(self.frame_basics, text="kbT")
-        temperature = self.temperature.get()
         self.temperautre_energy = ttk.Label(
-            self.frame_basics, text="{:.3g}".format(float(temperature) * self.kb_eV)
+            self.frame_basics, text="{:.3g}".format(self.init_value.temperature_eV)
         )
         # The second row
         self.deposition_rate_label = ttk.Label(
             self.frame_basics, text="Dep. rate(ML/min)"
         )
         self.deposition_rate = ttk.Entry(self.frame_basics, width=7)
-        self.deposition_rate.insert(tk.END, init_values["dep_rate"])
+        self.deposition_rate.insert(tk.END, self.init_value.dep_rate)
         self.deposition_rate.bind("<Return>", self.update_click)
         self.deposition_time_label = ttk.Label(self.frame_basics, text="Dep. time(min")
         self.deposition_time = ttk.Entry(self.frame_basics, width=7)
-        self.deposition_time.insert(tk.END, init_values["dep_time"])
+        self.deposition_time.insert(tk.END, self.init_value.dep_time)
         self.deposition_time.bind("<Return>", self.update_click)
         self.postanneal_time_label = ttk.Label(
             self.frame_basics, text="Post aneeal time (min)"
         )
         self.postanneal_time = ttk.Entry(self.frame_basics, width=7)
-        self.postanneal_time.insert(tk.END, init_values["post_anneal"])
+        self.postanneal_time.insert(tk.END, self.init_value.post_anneal)
         self.postanneal_time.bind("<Return>", self.update_click)
         self.prefactor_label = ttk.Label(self.frame_basics, text="Prefactor(1/s)")
         self.prefactor = ttk.Entry(self.frame_basics, width=7)
-        self.prefactor.insert(tk.END, init_values["prefactor"])
+        self.prefactor.insert(tk.END, self.init_value.prefactor)
         self.prefactor.bind("<Return>", self.update_click)
 
     def create_layout_basic(self):
@@ -178,9 +153,7 @@ class Window(ttk.Frame):
             "Si_inter",
             "Ag_top",
         ]
-        e_values: List[float] = [
-            init_values[self.labels[i]] for i in range(1, 10)
-            ]
+
         self.energylabels = []
         self.energies = []
         self.rate_labels = []
@@ -189,8 +162,8 @@ class Window(ttk.Frame):
                 ttk.Entry(self.frame_energies, width=7)
             )  # entryを9つ生成。_が変数だが、使われていない。
             self.rate_labels.append(ttk.Label(self.frame_energies, text="0"))
-        for i, energy in enumerate(self.energies):  # 上で作成したentryそれぞれに値を格納
-            energy.insert(tk.END, e_values[i])
+        for (energy, (key, val)) in zip(self.energies, self.init_value.binding_energies.items()):  # 上で作成したentryそれぞれに値を格納
+            energy.insert(tk.END, val)
             energy.bind("<Return>", self.update_click)
         for label in self.labels:
             self.energylabels.append(ttk.Label(self.frame_energies, text=label))
@@ -218,7 +191,7 @@ class Window(ttk.Frame):
             self.frame_checks, variable=self.bln_tr
         )
         self.transformation = ttk.Entry(self.frame_checks, width=7)
-        self.transformation.insert(tk.END, init_values["transformation"])
+        self.transformation.insert(tk.END, self.init_value.transformation)
         self.bln_defect = tk.BooleanVar()
         self.bln_defect.set(True)
         self.chk_defect_label = ttk.Label(
@@ -236,13 +209,13 @@ class Window(ttk.Frame):
     def create_widgets_records(self):
         self.record_label = tk.Label(self.frame_records, text="Record")
         self.record = tk.Entry(self.frame_records, width=50)
-        self.record.insert(tk.END, init_values["record_name"])
+        self.record.insert(tk.END, self.init_value.record_name)
         self.image_rec_label = tk.Label(self.frame_records, text="Image rec. (%)")
         self.image_rec = tk.Entry(self.frame_records, width=10)
-        self.image_rec.insert(tk.END, init_values["img_per"])
+        self.image_rec.insert(tk.END, self.init_value.img_per)
         self.comments_label = tk.Label(self.frame_records, text="Comments")
         self.comments = tk.Entry(self.frame_records, width=80)
-        self.comments.insert(tk.END, init_values["comments"])
+        self.comments.insert(tk.END, self.init_value.comments)
 
     def create_layout_records(self):
         self.record_label.grid(row=0, column=0, **self.padWE)
@@ -303,45 +276,38 @@ class Window(ttk.Frame):
         self.progress_bar.grid(pady=20)
 
     def close_function(self):
-        # plt.close("all")  <<< FIXME
         self.quit()
 
     def run(self):
         self.mainloop()
 
     def update_values(self):
-        temperature: str = self.temperature.get()
-        kbt = float(temperature) * self.kb_eV
+        self.init_value.n_cell_init = int(self.n_cell.get())
+        self.init_value.z_unit_init = int(self.z_unit.get())
+        self.init_value.temperature = float(self.temperature.get())
+        self.init_value.dep_rate = float(self.deposition_rate.get())
+        self.init_value.dep_time = float(self.deposition_time.get())
+        self.init_value.post_anneal = float(self.postanneal_time.get())
+        self.init_value.prefactor = float(self.prefactor.get())
+        for energy_entry, key in zip(self.energies, self.init_value.binding_energies):
+            self.init_value.binding_energies[key] = float(energy_entry.get())
+        #
+        kbt = self.init_value.temperature_eV
+        self.temperautre_energy["text"] = "{:.3g}".format(kbt)
         for energy, rate_label in zip(self.energies, self.rate_labels):
-
             rate_label["text"] = str(
                 "{:.3g}".format(
                     rate(float(self.prefactor.get()), kbt, float(energy.get()))
                 )
             )
         self.update()
-        #update dictionaly
-        init_values["n_cell_init"] = self.n_cell.get()
-        init_values["z_unit_init"] = self.z_unit.get()
-        init_values["temperature"] = self.temperature.get()
-        init_values["dep_rate"] = self.deposition_rate.get()
-        init_values["dep_time"] = self.deposition_time.get()
-        init_values["post_anneal"] = self.postanneal_time.get()
-        init_values["prefactor"] = self.prefactor.get()
-        init_values["transformation"] = self.transformation.get()
-        init_values["record_name"] = self.record.get()
-        init_values["img_per"] = self.image_rec.get()
-        init_values["comments"] = self.comments.get()
-        init_values["kbt"] = kbt
-        for i in range(1,10):
-            init_values[self.labels[i]] = self.energies[i-1].get()
 
     def update_click(self, event):
         self.update_values()
 
     def lattice_check(self):
-        lattice_formed = lattice_form(init_values)
-        lattice_visual(init_values, lattice_formed[0], lattice_formed[1])
+        lattice_formed = lattice_form(self.init_value)
+        lattice_visual(self.init_value, lattice_formed[0], lattice_formed[1])
 
     def start_function(self):
         if self.var_method.get() == "Check lattice":
@@ -369,9 +335,10 @@ class Window(ttk.Frame):
         #cal_start()
         """
 
-
+"""
 if __name__ == "__main__":
     application = tk.Tk()
     application.title("kMC_Si")
     Window(application)
     application.mainloop()
+"""
