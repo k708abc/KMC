8038e8f27767e1ec677dcb4809a7c79f0748904c
k708 <k708abc@yahoo.co.jp>
2020-11-20 02:34:55 +0900

    end of the day (middle)

--- event_collection.py
+++ event_collection.py
@@ -68,12 +68,30 @@ def get_energy(atom_set, bonds, target, params):
     return energy
 
 
-def possible_events(atom_set, bonds, target, params, energy):
+def possible_events(atom_set, bonds, target, params, energy, unit_length):
     events: List[tuple] = []
     rates: List[float] = []
-    z_judge = target[2]
+    #
+    atom_x = target[0]
+    atom_y = target[1]
+    atom_z = target[2]
+    # nnn: next nearest neighbor
+    site_nnn = [
+        ((atom_x - 1) % unit_length, atom_y, atom_z),
+        (atom_x, (atom_y - 1) % unit_length, atom_z),
+        ((atom_x + 1) % unit_length, atom_y, atom_z),
+        (atom_x, (atom_y + 1) % unit_length, atom_z),
+        ((atom_x + 1) % unit_length, (atom_y - 1) % unit_length, atom_z),
+        ((atom_x - 1) % unit_length, (atom_y + 1) % unit_length, atom_z),
+        ]
 
-    if z_judge == 0:
+    if z_judge == 0:    # 0層目
+        nn_atoms = []
+        nn_nn_atoms = []
+
+        for bond_nn in bonds[target]:
+            
+
         for bond_nn in bonds[target]:
             if atom_set[bond_nn] == 0:  # empty neighbor site
                 events.append(bond_nn)
@@ -91,7 +109,18 @@ def possible_events(atom_set, bonds, target, params, energy):
                         # 隣接原子の真上が候補
                         events.append((bond_nn[0], bond_nn[1], bond_nn[2] + 1))
                         rates.append(rate(energy))
-    elif z_judge == 1:
+
+
+
+
+
+
+
+
+
+
+
+    elif z_judge%2 == 1:
         for bond_nn in bonds[target]:
             if atom_set[bond_nn] == 0 and bond_nn[2] == 0:
                 # empty neighbor site in lower layer
@@ -113,15 +142,17 @@ def possible_events(atom_set, bonds, target, params, energy):
 
 
 
-                    
+
     elif z_judge == 2:
 
 
 def get_events(atom_set, bonds, target, params):
     events: List[tuple] = []
     rates: List[float] = []
+    unit_length = param.n_cell_init
     # calculate total energy
     energy = get_energy(atom_set, bonds, target, params)
-    event, rates = possible_events(atom_set, bonds, target, params, energy)
+    # calculate possible events
+    event, rates = possible_events(atom_set, bonds, target, params, energy, unit_length)
 
     return events, rates
