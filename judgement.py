72daac600bd0a1c65f37434c1534f19066423d02
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2020-12-19 16:41:55 +0900

    Is it better for speed?

--- judgement.py
+++ judgement.py
@@ -2,10 +2,9 @@ import random
 
 
 def judge_null(success: float) -> str:
-    if success > 1:
-        print("Error: probablity over 1")
+    assert success <= 1
     null = 1 - success
-    result = ["success", "failure"]
+    result = [True, False]
     weight = [success, null]
     judge = random.choices(result, weights=weight)
     return judge[0]
