a9e98b773dc4782a98a65a64aeecdb27413c70b4
k708 <k708abc@yahoo.co.jp>
2021-04-14 23:29:29 +0900

    change heatap etc.

--- Modules/heatmap.py
+++ Modules/heatmap.py
@@ -1,10 +1,77 @@
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
-from pptx import Presentation
-from pptx.util import Inches, Pt
-import os
+
+# from pptx import Presentation
+# from pptx.util import Inches, Pt
+# import os
 
 
+def heatmap_image(dx, dy, xlist, ylist, modelist):
+    fig = plt.figure(figsize=(6, 6))
+    ax = fig.add_subplot(111)
+    for x, y, mode in zip(xlist, ylist, modelist):
+        RGB = [0, 0, 0]
+        if mode == "DW":
+            RGB = [1, 0, 0]
+
+        elif mode == "BL":
+            RGB = [0.1, 0.1, 0.1]
+
+        elif mode == "SK":
+            RGB = [0, 1, 0]
+
+        elif mode == "FM":
+            RGB = [0, 0, 1]
+
+        elif mode == "VW":
+            RGB = [1, 1, 0]
+
+        r = patches.Rectangle(
+            xy=(x - dx / 2, y - dy / 2),
+            width=dx,
+            height=dy,
+            color=RGB,
+            fill=True,
+        )
+
+        ax.add_patch(r)
+
+    ax.set_xlim(min(xlist) - dx / 2, max(xlist) + dx / 2)
+    ax.set_ylim(min(ylist) - dy / 2, max(ylist) + dy / 2)
+    ax.set_xticks(xlist)
+    ax.set_yticks(ylist)
+    ax.set_xlabel("First layer", fontsize=16)
+    ax.set_ylabel("Multi layer", fontsize=16)
+    labels = ax.get_xticklabels()
+    plt.setp(labels, rotation=45, fontsize=16)
+    plt.tick_params(labelsize=16)
+    plt.tight_layout()
+    #
+    heat_name = "heatmap.png"
+    plt.savefig(heat_name)
+
+
+def rec_modes(xlist, ylist, modelist):
+    file_name = "growth_modes.txt"
+    with open(file_name, "a") as f:
+        for x, y, mode in zip(xlist, ylist, modelist):
+            print(str(x) + "\t" + str(y) + "\t" + mode, file=f)
+
+
+def form_heatmap(growth_mode, rec_name, E1, E2, diff1, diff2):
+    xlist = []
+    ylist = []
+    modelist = []
+    for i in range(len(E1)):
+        for k in range(len(E2)):
+            xlist.append(E1[i])
+            ylist.append(E2[k])
+            modelist.append(growth_mode[i][k][3])
+    heatmap_image(diff1, diff2, xlist, ylist, modelist)
+    rec_modes(xlist, ylist, modelist)
+
+
+"""
 def form_heatmap(growth_mode, rec_name, E1, E2, diff1, diff2):
     fig = plt.figure(figsize=(6, 6))
     ax = fig.add_subplot(111)
@@ -109,3 +176,4 @@ def form_heatmap(growth_mode, rec_name, E1, E2, diff1, diff2):
         left = Inches(0)
         slide.shapes.add_picture(heat_name, left, top, height=height)
         prs.save(ppt_name)
+"""
