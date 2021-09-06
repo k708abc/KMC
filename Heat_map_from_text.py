b1540f0beff4eb6bc97dd6e3881e665f0d78bd22
k708 <k708abc@yahoo.co.jp>
2021-09-06 17:24:58 +0900

    Minor change

--- Heat_map_from_text.py
+++ Heat_map_from_text.py
@@ -1,9 +1,17 @@
-from Modules.heatmap import heatmap_image
+from Modules.heatmap import heatmap_image, form_examples
 
 
 xlist = []
 ylist = []
 modelist = []
+RGB_set = [
+    [1, 0, 0],
+    [0.1, 0.1, 0.1],
+    [0.21, 0.75, 0.4],
+    [0.7, 0.7, 0.7],
+    [0, 0, 1],
+]
+patterns = ["/", None, "\\", ".", "x"]
 
 with open("growth_modes.txt", "r") as f:
     for line in f:
@@ -18,6 +26,7 @@ with open("growth_modes.txt", "r") as f:
             modelist.append(line_s[2].replace("\n", ""))
 
 
-heatmap_image(dx, dy, xlist, ylist, modelist)
+heatmap_image(dx, dy, xlist, ylist, modelist, RGB_set, patterns)
+form_examples(RGB_set, patterns)
 print("heat map is formed")
 input()
