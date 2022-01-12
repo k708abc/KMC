734d6e770666d6b0ffd6ab2c45fd09cb90e6abe0
k708 <k708abc@yahoo.co.jp>
2022-01-12 13:47:21 +0900

    Main_keepをlistdirから削除

--- run_job.py
+++ run_job.py
@@ -1,48 +1,48 @@
-#python script to run jobs in IMS-server
+# python script to run jobs in IMS-server
 import os
 import glob
 import shutil
 
-njobs=5
+njobs = 5
 
-listdirs=['Main_keep','Modules','Test_modules']
-inputdir='Initial_inputs'
-execfile=glob.glob('*.py')
+listdirs = ["Modules", "Test_modules"]
+inputdir = "Initial_inputs"
+execfile = glob.glob("*.py")
 
-root=os.getcwd()
-for i in range(1,njobs+1):
-    dirname='job-'+str(i)
-    os.makedirs(dirname,exist_ok=True)
+root = os.getcwd()
+for i in range(1, njobs + 1):
+    dirname = "job-" + str(i)
+    os.makedirs(dirname, exist_ok=True)
     os.chdir(dirname)
 
-    with open('run.sh','w') as js:
-        js.write('#!/bin/csh \n')
-        js.write('#$ -cwd \n')
-        js.write('#$ -V -S /bin/bash \n')
-        js.write('#$ -N KMC-job-'+str(i)+'\n')
-        js.write('#$ -o stdout \n')
-        js.write('#$ -e stderr \n')
-        js.write('#$ -q all.q \n')
-        js.write('#$ -pe x24 24 \n')
-        js.write('export OMP_NUM_THREADS=1 \n')
-        js.write('python RFKMC_multi.py \n')
+    with open("run.sh", "w") as js:
+        js.write("#!/bin/csh \n")
+        js.write("#$ -cwd \n")
+        js.write("#$ -V -S /bin/bash \n")
+        js.write("#$ -N KMC-job-" + str(i) + "\n")
+        js.write("#$ -o stdout \n")
+        js.write("#$ -e stderr \n")
+        js.write("#$ -q all.q \n")
+        js.write("#$ -pe x24 24 \n")
+        js.write("export OMP_NUM_THREADS=1 \n")
+        js.write("python RFKMC_multi.py \n")
 
-    #copy required
+    # copy required
 
     for dir in listdirs:
-        origin=os.path.join(root,dir)
-        shutil.copytree(origin, './'+dir)
+        origin = os.path.join(root, dir)
+        shutil.copytree(origin, "./" + dir)
 
-    #input
-    inputname='kmc_input_{0}.yml'.format(i)
-    origin=os.path.join(root,inputdir,inputname)
-    shutil.copy(origin,'./kmc_input.yml')
+    # input
+    inputname = "kmc_input_{0}.yml".format(i)
+    origin = os.path.join(root, inputdir, inputname)
+    shutil.copy(origin, "./kmc_input.yml")
 
-    #exec
+    # exec
     for file in execfile:
-        origin=os.path.join(root,file)
-        shutil.copy(origin,file)
+        origin = os.path.join(root, file)
+        shutil.copy(origin, file)
 
-    os.system('qsub run.sh')
+    os.system("qsub run.sh")
 
     os.chdir(root)
